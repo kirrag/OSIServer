2d404d858723c561d1cad3351dc25c2a84e8554e
Garrik Ten <ten@otux.ru>
2023-03-07 00:48:20 +0300

    OSIServer v2.0

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,9 +3,11 @@ package ru.netology;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
 
 public class Main {
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) throws Exception {
 		System.out.println("server started");
 		int port = 8089;
 
@@ -18,9 +20,24 @@ public class Main {
 					System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
 
 					final String name = in.readLine();
-					out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+					if(name.equals("Client")) {
+						TimeUnit.SECONDS.sleep(4);
+						out.println(String.format("Hi %s from port %d, I'm Server from port %d", name, clientSocket.getPort(), port));
+					}
+
+					final String req1 = in.readLine();
+					if(req1.equals("req1")) {
+						TimeUnit.SECONDS.sleep(4);
+						out.println("Wow, you're too young for me, I only support http 1.0 protocol.");
+					} 
+
+					final String req2 = in.readLine();
+					if (req2.equals("req2")) {
+						TimeUnit.SECONDS.sleep(4);
+						out.println("By!");
+					}
 				}
-				
 			}
 		}
     }
